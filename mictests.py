2e1246d9d50f75251337eb7f3f5ef156e71e69f6
SupaFasJellyFish <lhogan63@gmail.com>
2023-06-03 16:51:59 -0400

    significantly expanded mictests.py to test APIs

--- mictests.py
+++ mictests.py
@@ -5,7 +5,7 @@ r = sr.Recognizer()
 mic = sr.Microphone(device_index=2)
 
 #instantiate interpretation log file
-sttlog = open("speechtotextlog.txt","w")
+sttlog = open("speechtotextlogwhisper.txt","w")
 
 #configure the recognizer instance
 #make the pause threshold longer because the default cuts off at "no defects"
@@ -20,9 +20,11 @@ try:
         print("heard, processing...")
         #interpret the audio, using a try to handle case where STT fails
         try:
-            # TODO swap speech recognition to Vosk
-            defectbroadcast = r.recognize_google(audio)
-            sttlog.write(defectbroadcast + "\n")
+            #trying a number of different stt models... Trying to find best for purpose.
+            #defectbroadcast = r.recognize_vosk(audio)
+            #defectbroadcast = r.recognize_google(audio)
+            defectbroadcast = r.recognize_whisper(audio, model = "small") #model attribute is whichever is preferred in the OpenAI Whisper docs. Default is base for SpeechRecognition library.
+            sttlog.write(defectbroadcast + "n")
             print("interpreted as " + defectbroadcast)
         except:
             print("there was an error interpreting the speech. Please try again.")
