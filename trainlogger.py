a6275951426e995297de54e2784012c011a7d08b
Luke Hogan <lhogan63@gmail.com>
2023-06-16 21:20:28 -0400

    TrainLogger file completed, ready for debug.

--- trainlogger.py
+++ trainlogger.py
@@ -1,6 +1,6 @@
 import speech_recognition as sr
 import sqlite3
-import string
+from datetime import datetime
 
 #dict for common number misinterpretations by Whisper STT.
 worddigit = {"to":"2","too":"2","niner":"9","9R":"9"}
@@ -8,54 +8,79 @@ worddigit = {"to":"2","too":"2","niner":"9","9R":"9"}
 #Table for letter and punctuation removal operations
 filtertable = str.maketrans("","","abcdefghijklmnopqrstuvwxyz.,? !")
 
-#Template defect detector broadcast:
-#CSX equipment defect detector milepost 5.3 track 1 No Defects No Defects Total Axle three eight four end of transmission
+#Class to store data about a train
+class train:
+    def __init__(self, length, track, defect):
+        self.length = length
+        self.track = track
+        self.defect = defect
 
-#SQL table: Timestamp(datetime)    Track(number)   Length(number)  Defects(text)   
+#SQL table schema (for reference):CREATE TABLE trains( timestamp DATETIME, axles INTEGER, defect TEXT NOT NULL, broadcast TEXT NOT NULL, track TEXT_NOT_NULL); 
 
-#define function to clean up a string containing numbers to be left with only numbers.
+# TODO Prep sql database for I/O
+dbconnection = sqlite3.connect("trains.db")
+dbcursor = dbconnection.cursor()
+
+#Set up mic and recognizer objects for speech recognition
+r = sr.Recognizer()
+audiosource = sr.Microphone(device_index=2) #Change device_index to match the desired audio source in the pyaudio list.
+
+
+#Function to clean up a string containing numbers to be left with only numbers.
 def stringtonum(input):
-    # TODO search string using the worddigit dictionary, do find and replace on words in the string
+    #search string using the worddigit dictionary, do find and replace on words in the string
     for old, new in worddigit.items():
         input = input.replace(old, new)
-    # TODO eliminate extraneous letters and punctuation
+    #eliminate extraneous letters and punctuation
     numstring = input.translate(filtertable)
     return int(numstring)
 
-#logic to get string from RTL-SDR audio input
+#Function to interpret the defect detector broadcast
+def interpret(broadcast):
+    #Get the track number. Simplified logic; your defect detector may behave differently. Be sure to check if adapting this on your own. My local defect detector only broadcasts
+    #"Track 1". Otherwise, if the train is on track 2, it doesn't broadcast the track number.
+    if broadcast.find("track") > 0:
+        track = 1
+    else:
+        track = 2
+
+    #Determine if there are no defects
+    if broadcast.find("no") > 0:
+        defectval="NO"
+    else:
+        defectval="YES"
+        # TODO save the broadcast string to the defect database
+
+    #Get the axle count. Needs to be fed a lowercase string.
+    axlecountstr = broadcast.split("axle ")[1].split(" end of transmission")[0]
+    # TODO filter the number string to get the mix of numbers, punctuation and words down to an integer.
+    if axlecountstr.isnumeric():
+        axlecount = int(axlecountstr)
+    else:
+        #Filter the string down to numbers.
+        axlecount = stringtonum(axlecountstr)
+    return train(axlecount,track,defectval)
+
+while(True):
+    try:
+        #logic to get string from RTL-SDR audio input
+        print("Listening for a broadcast...")
+        audio = r.listen(audiosource,timeout = None)
+        traintime = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        print("Processing broadcast.")
+        broadcast = r.recognize_whisper(audio, model = "small").lower() #Make the string lowercase to make processing easier.
+
+        #Logic to check if a broadcast was full or not.
+        if broadcast.find("equipment") * broadcast.find("transmission") > 1:
+            newtrain = interpret(broadcast)
+            #Store the info about the train into the database
+            dbcursor.execute("INSERT INTO trains (timestamp, axles, defects, broadcast, track) VALUES (?,?,?,?,?)", [traintime, newtrain.length, newtrain.defect, broadcast, newtrain.track])
+            dbconnection.commit()
 
 
-#logic to parse returned string
-
-# TODO Create logic here that checks to see if the string is a legit broadcast or something else on the frequency
-
-splsourcestring = "csx equipment defect detector milepost 5.3 track 1 no defects no defects total axle three eight four end of transmission"
-sourcestring = splsourcestring
-
-#Get the track number. Simplified logic; your defect detector may behave differently. Be sure to check if adapting this on your own. My local defect detector only broadcasts
-#"Track 1". Otherwise, if the train is on track 2, it doesn't broadcast the track number.
-if sourcestring.find("track") != 1:
-    track = 1
-else:
-    track = 2
-
-
-#Determine if there are no defects
-if sourcestring.find("no") != -1:
-    defectval="NO"
-else:
-    defectval="YES"
-    # TODO save the broadcast string somewhere for further analysis. 
-
-#Get the axle count
-axlecountstr = sourcestring.split("axle ")[1].split(" end of transmission")[0]
-# TODO filter the number string to get the mix of numbers, punctuation and words down to an integer.
-if axlecountstr.isnumeric():
-    axlecount = int(axlecountstr)
-else:
-    #Filter the string down to numbers.
-    axlecount = stringtonum(axlecountstr)
-
-#Store the info about the train into the database
+    except KeyboardInterrupt:
+        #close access to database
+        dbconnection.close()
+        print("TrainLogger closed successfully.")
 
 
